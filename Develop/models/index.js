7642af7e76bad7d41ae91bc98926fed1f87a4f59
Luc Burns <burnsluc2@gmail.com>
2021-04-21 22:19:42 -0400

    Fix

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -14,7 +14,7 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
+  // onDelete: "CASCADE",
   as: "product_category"
 });
 
